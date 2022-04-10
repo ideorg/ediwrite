7c726dcf5f52800703b4bb6fd679d1bcec6fc633
Andrzej Borucki <borucki.andrzej@gmail.com>
2022-04-10 13:45:57 +0200

    read screen geometry to establish application window size

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 
 #include <QApplication>
 #include <QCommandLineParser>
+#include <QScreen>
 
 int main(int argc, char **argv)
 {
@@ -18,8 +19,13 @@ int main(int argc, char **argv)
     parser.addPositionalArgument(QStringLiteral("source"), QStringLiteral("The source file to highlight."));
     parser.process(app);
 
+    QScreen *screen = QGuiApplication::primaryScreen();
+    QRect  screenGeometry = screen->geometry();
+    int height = screenGeometry.height();
+    int width = screenGeometry.width();
+
     CodeEditor edit;
-    edit.resize(1024, 1024);
+    edit.resize(int(screenGeometry.width()*0.8), int(screenGeometry.height()*0.8));
     edit.show();
     if (parser.positionalArguments().size() == 1) {
         edit.openFile(parser.positionalArguments().at(0));
